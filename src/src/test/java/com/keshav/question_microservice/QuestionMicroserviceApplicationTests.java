c8feebc61a740700c10b929887a9d177fc6aec80
keshavp07-git <keshav.prajapati830@gmail.com>
2025-02-03 20:11:03 +0530

    Question-Service

---
+++ src/src/test/java/com/keshav/question_microservice/QuestionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keshav.question_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
